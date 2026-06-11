6899d928bd1eac9708788d15b48b5c7aec989677
Anonymous <anonymous@example.com>
2026-06-11 15:55:12 +0200

    Adding Properties for Stats

--- sim4da-S26-pingpong/build.gradle.kts
+++ sim4da-S26-pingpong/build.gradle.kts
@@ -30,3 +30,19 @@ tasks.named<JavaExec>("run") {
     // Forward CLI args: ./gradlew run --args="20"
     standardInput = System.`in`
 }
+
+tasks.register<JavaExec>("runTokenRing") {
+    group = "application"
+    description = "Startet die Token-Ring-Simulation"
+
+    // Pfad zu deiner Hauptklasse
+    mainClass.set("tokenring.TokenRingSimulation")
+
+    // Nutzt den Runtime-Classpath inklusive sim4da.jar
+    classpath = sourceSets["main"].runtimeClasspath
+
+    // Ermöglicht es, Argumente via --args zu übergeben
+    if (project.hasProperty("args")) {
+        args(project.property("args").toString().split(" "))
+    }
+}
